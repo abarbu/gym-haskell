d32bb3bfc67888eea917152159aea1f32e604c8e
Andrei <andrei@0xab.com>
2021-12-16 16:13:41 -0500

    Cleanup shell.nix detritus

--- shell.nix
+++ shell.nix
@@ -4,17 +4,6 @@ args@{ sources ? (import nix/sources.nix {}),
 let
   package = import ./default.nix args;
   pkgs = package.pkgs;
-  # mesaX = pkgs.mesa.override { enableOSMesa = true; };
-  # https://github.com/siddharthverma314/pyrl/
-  # glfw = pkgs.python38Packages.callPackage ./nix/glfw.nix { inherit sources; };
-  # mujoco = pkgs.python38Packages.callPackage ./nix/mujoco.nix { inherit sources; };
-  # mesa = pkgs.mesa.override { enableOSMesa = true; };
-  # mujoco-py = pkgs.python38Packages.callPackage ./nix/mujoco-py.nix {
-  #   inherit sources withCuda pkgs mesa mujoco;
-  # };
-  # gym-super-mario-bros = pkgs.python38Packages.callPackage ./nix/gym-super-mario-bros.nix {
-  #   inherit nes-py;
-  # };
   buildInputs = [ pkgs.python38Packages.python
                   pkgs.python38Packages.gym
                   pkgs.python38Packages.pyglet
@@ -54,10 +43,6 @@ let
                 ];
   stdenv = pkgs.stdenv;
 in
-# stdenv.mkDerivation {name = "outer";dontUnpack = true; installPhase = ""; shellHook = "TEST1=1"; buildInputs = [(stdenv.mkDerivation { name = "inner";dontUnpack = true; shellHook = "exit 1"; installPhase = ""; })];}
-
-# pkgs.mujoco-py
-
 package.shellFor {
   tools = {
     cabal = "latest";
@@ -66,8 +51,6 @@ package.shellFor {
     ormolu = "latest";
     haskell-language-server = "latest";
   };
-  # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs + ":" + pkgs.mujoco + "/bin";
-  # MUJOCO_PY_MUJOCO_PATH = pkgs.mujoco;
   exactDeps = true;
   inherit buildInputs;
 }
